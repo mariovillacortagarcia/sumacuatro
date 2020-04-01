63dee13fcc97671da76a8e265231e7c441460ce0
mariohohoho <mariovillacortagarcia@gmail.com>
2020-04-01 12:36:14 +0200

    Suma4 canonico

--- script/efectos.js
+++ script/efectos.js
@@ -1,6 +1,7 @@
 var ronda = 1;
 var puntuacion = 0;
-var tiempo = 30;
+var tiempo = 180;
+var tiempoEsconder = 6000;
 var estado = [0, 0, 0, 0];
 var temporizador;
 
@@ -20,10 +21,9 @@ $(document).ready(function() {
 
 function start() {
   //Cabecera
-  console.log(tiempo);
   temporizador = setInterval(function() { //Temporizador
     if (tiempo < 0) { //Fin de la partida
-      alert("Fin de la partida. Puntuación: "+puntuacion);
+      alert("Fin de la partida. Puntuación: " + puntuacion);
       clearInterval(temporizador);
     }
     var timing = new Date(0);
@@ -43,40 +43,12 @@ function start() {
   actualizaEntrada();
   limpiaInput();
 
-}
+  //Esconde numeros
+  setTimeout(function() {
+    escondeEntrada();
+  }, tiempoEsconder);
 
-function actualizaEntrada() {
-  for (var j = 0; j < 4; j++) {
-    var elem = "#elem" + String(j + 1) + "_in";
-    estado[j] = Math.round(Math.random() * 10);
-    for (var k = 0; k < j; k++) {
-      while (estado[k] === estado[j]) { //Asegura numeros distintos
-        estado[j] = Math.round(Math.random() * 10);
-      }
-    }
-    $(elem).empty();
-    $(elem).append("<h3> " + String(estado[j]) + "</h3>");
-  }
 
-}
-
-function actualizaPuntuacion() {
-  $("#puntuacion").empty();
-  $("#puntuacion").append("Puntuación: " + String(puntuacion));
-}
-
-function actualizaRonda() {
-  $("#ronda").empty();
-  $("#ronda").append("Ronda " + String(ronda));
-}
-
-function limpiaInput(){
-  for (var i = 0; i < 4; i++) {
-    var elem = "#elem" + String(i + 1) + "_out";
-      $(elem).removeClass("text-success");
-      $(elem).removeClass("text-danger");
-      $(elem).val('');
-  }
 }
 
 function check() {
@@ -93,10 +65,71 @@ function check() {
   actualizaPuntuacion();
   setTimeout(function() {
     ronda++;
+    reduceTiempoEsconder();
+    console.log(tiempoEsconder);
     start();
 
   }, 1500);
 
 
 
+}
+
+function escondeEntrada() {
+  for (var j = 0; j < 4; j++) {
+    var elem = "#elem" + String(j + 1) + "_in";
+    $(elem).empty();
+    $(elem).append("<h3>???</h3>");
+  }
+}
+
+function muestraEntrada() {
+  for (var j = 0; j < 4; j++) {
+    var elem = "#elem" + String(j + 1) + "_in";
+    $(elem).empty();
+    $(elem).append("<h3> " + String(estado[j]) + "</h3>");
+  }
+}
+
+function actualizaEntrada() {
+  for (var j = 0; j < 4; j++) {
+    var elem = "#elem" + String(j + 1) + "_in";
+    estado[j] = Math.round(Math.random() * 10);
+    for (var k = 0; k < j; k++) {
+      while (estado[k] === estado[j]) { //Asegura numeros distintos
+        estado[j] = Math.round(Math.random() * 10);
+      }
+    }
+    muestraEntrada();
+  }
+
+}
+
+function actualizaPuntuacion() {
+  $("#puntuacion").empty();
+  $("#puntuacion").append("Puntuación: " + String(puntuacion));
+}
+
+function actualizaRonda() {
+  $("#ronda").empty();
+  $("#ronda").append("Ronda " + String(ronda));
+}
+
+function limpiaInput() {
+  for (var i = 0; i < 4; i++) {
+    var elem = "#elem" + String(i + 1) + "_out";
+    $(elem).removeClass("text-success");
+    $(elem).removeClass("text-danger");
+    $(elem).val('');
+  }
+}
+
+function reduceTiempoEsconder() {
+  if (tiempoEsconder > 3000) {
+    tiempoEsconder -= 250;
+  } else if (tiempoEsconder > 1500) {
+    tiempoEsconder -= 200;
+  } else if (tiempoEsconder > 500){
+    tiempoEsconder -= 100;
+  }
 }
