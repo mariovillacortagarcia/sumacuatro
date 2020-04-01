d1db010aef380d0f5dd07048fac35c1d2f5f34ff
mariohohoho <mariovillacortagarcia@gmail.com>
2020-04-01 10:29:17 +0200

    Añadido temporizador de ronda

--- script/efectos.js
+++ script/efectos.js
@@ -10,14 +10,23 @@ $(document).ready(function() {
 
 function start() {
   var ronda = 1;
-  while(true) {
-    $("#ronda").append("Ronda "+String(ronda));
+  while (true) {
+    tiempo = 0;
+    setInterval(function() {
+      var timing = new Date(0);
+      timing.setSeconds(tiempo);
+      timing = timing.toISOString().substr(14, 5)
+      $("#temporizador").empty();
+      $("#temporizador").append(timing);
+      tiempo++;
+    }, 1000);
+    $("#ronda").append("Ronda " + String(ronda));
     var estado = [0, 0, 0, 0];
     //Inicializacion de los numeros
     for (var j = 0; j < 4; j++) {
       estado[j] = Math.round(Math.random() * 9);
-      for(var k = 0; k<j; k++){
-        while(estado[k] === estado[j]){   //Asegura numeros distintos
+      for (var k = 0; k < j; k++) {
+        while (estado[k] === estado[j]) { //Asegura numeros distintos
           estado[j] = Math.round(Math.random() * 9);
         }
       }
